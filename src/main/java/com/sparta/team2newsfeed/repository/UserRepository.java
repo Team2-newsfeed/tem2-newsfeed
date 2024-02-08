82213fe9304ae5234ef2b12f93e753f433b25c5b
mine <mineki94@naver.com>
2024-02-08 14:54:08 +0900

    필요없는 코드 삭제

--- src/main/java/com/sparta/team2newsfeed/repository/UserRepository.java
+++ src/main/java/com/sparta/team2newsfeed/repository/UserRepository.java
@@ -12,6 +12,4 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
-
-    void update(User user, UserUpdateRequestDto userUpdateRequestDto, PasswordEncoder passwordEncoder);
 }
